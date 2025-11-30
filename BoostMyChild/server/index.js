62f75ded6ffd87422d8faf84005fef59ef15e23c
Prathmesh <126573912+prathamesh3245@users.noreply.github.com>
2025-12-01 00:46:42 +0530

    Configure CORS for public deployment

--- BoostMyChild/server/index.js
+++ BoostMyChild/server/index.js
@@ -8,6 +8,13 @@ const app = express()
 app.use(cors());
 app.use(express.json());
 
+app.use(cors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true
+}));
+
+
 app.use('/api/events', eventRoutes);
 
 mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/Calendar')
