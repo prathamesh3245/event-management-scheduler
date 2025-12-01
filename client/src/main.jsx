ff878bea994a1a580954b9a15ca07ea591c364c2
Prathmesh <126573912+prathamesh3245@users.noreply.github.com>
2025-12-01 18:44:10 +0530

    Changed the path of fullcalendar import to correct path

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,8 +4,10 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
 
-import '@fullcalendar/common/main.css';
-import '@fullcalendar/daygrid/main.css';
+import '@fullcalendar/core';
+import '@fullcalendar/daygrid';
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
