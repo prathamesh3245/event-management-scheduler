88d7932845e6da6b2e4fbb9fd799e6c7214d47cc
Prathmesh <126573912+prathamesh3245@users.noreply.github.com>
2025-12-01 18:21:13 +0530

    Deployment Fix: Cleaned HTML and verified FullCalendar CSS JS imports.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,9 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
 
+import '@fullcalendar/common/main.css';
+import '@fullcalendar/daygrid/main.css';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
