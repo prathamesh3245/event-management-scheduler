d0e44a245152b1f15562c0eba9195ba2fb952bad
prathamesh3245 <126573912+prathamesh3245@users.noreply.github.com>
2025-12-01 21:22:51 +0530

    Delete server directory

--- server/routes/eventRoutes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const eventController = require('../controllers/eventController');
-
-router.get('/', eventController.getEvents);
-router.post('/', eventController.createEvent);
-router.put('/:id', eventController.updateEvent);
-router.delete('/:id', eventController.deleteEvent);
-
-module.exports = router;
